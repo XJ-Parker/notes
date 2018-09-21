82a2f7e1a118d4d0084cda7b94bc99afe1e7f58a
xujun <hbhgxujun@163.com>
2018-09-21 16:18:55 +0800

    简版vue

---
+++ demos/simple-vue/vue.js
@@ -0,0 +1,116 @@
+
+class Vue{
+  constructor(options={}){  // 构造函数 数据初始化
+    this.$el = document.querySelector(options.el)
+    let data = this.data = options.data
+    Object.keys(data).forEach((key) => {
+      this.proxyData(key)
+    });
+    this.methods = options.methods
+    this.watcherTask = {}
+    this.observer(data)
+    this.compile(this.$el)
+  }
+
+  proxyData(key){ //代理data
+    let that = this
+    Object.defineProperty(that,key,{
+      configurable: false,
+      enumerable: true,
+      get() {
+        return that.data[key]
+      },
+      set(newVal) {
+        that.data[key] = newVal
+      }
+    })
+  }
+
+  observer(data){
+    let that = this
+    Object.keys(data).forEach(key=>{
+      let value = data[key]
+      this.watcherTask[key] = []
+      Object.defineProperty(data,key,{
+        configurable: false,
+        enumerable: true,
+        get(){
+          return value
+        },
+        set(newVal){
+          if(newVal !== value){
+            value = newVal
+            that.watcherTask[key].forEach(task=>{
+              task.update()
+            })
+          }
+        }
+      })
+    })
+  }
+
+  compile(el){
+    let nodes = el.childNodes
+    for(let i=0; i<nodes.length; i++){
+      const node = nodes[i]
+      if(node.nodeType === 3){  // 文本
+  
+        let text = node.textContent.trim()
+        if(!text) continue;
+        this.compileText(node,'textContent')
+      }else if(node.nodeType === 1 ){  //元素
+        if(node.childNodes.length > 0){
+          this.compile(node)
+        }
+        if(node.hasAttribute('v-model') && (node.tagName === 'INPUT' || node.tagName === 'TEXTAREA')){
+          node.addEventListener('input',(()=>{
+            let attrVal = node.getAttribute('v-model')
+            this.watcherTask[attrVal].push(new Watcher(node,this,attrVal,'value'))
+            node.removeAttribute('v-model')
+            return ()=>{
+              this.data[attrVal] = node.value
+            }
+          })())
+        }
+        if(node.hasAttribute('v-html')){
+          let attrVal = node.getAttribute('v-html')
+          this.watcherTask[attrVal].push(new Watcher(node,this,attrVal,'innerHTML'))
+          node.removeAttribute('v-html')
+        }
+        if(node.hasAttribute('@click')){
+          let attrVal = node.getAttribute('@click')
+          node.addEventListener('click', e=>{
+            this.methods[attrVal] && this.methods[attrVal].bind(this)()
+          })
+        }
+      }
+    }
+  }
+
+  compileText(node,type){
+    let reg = /\{\{(.*)\}\}/g,txt = node.textContent
+    if(reg.test(txt)){
+      node.textContent = txt.replace(reg,(matched,value)=>{
+        let tpl = this.watcherTask[value] || []
+        tpl.push(new Watcher(node,this,value,type))
+        return value.split('.').reduce((val,key)=>{
+          return this.data[key]
+        },this.$el)
+      })
+    }
+  }
+}
+
+class Watcher{
+  constructor(el,vm,value,type){
+    this.el= el
+    this.vm= vm
+    this.value= value
+    this.type = type
+    this.update()
+  }
+
+  update(){
+    this.el[this.type] = this.vm.data[this.value]
+  }
+}
